a5d71fe674b5641f552d571fc2ab40e7d4f79800
Hasil Trivedi <PCA56@server7.com>
2026-06-15 16:50:26 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+builder.Services.AddControllers();
+
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+app.MapControllers();
+app.Run();
